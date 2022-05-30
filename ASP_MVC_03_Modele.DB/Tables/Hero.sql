ccb247d2022c55a126d4d79cc34308ef4587374f
Aurélien S <aurelien.strimelle@bstorm.be>
2022-05-30 11:38:38 +0200

    Init Database

---
+++ ASP_MVC_03_Modele.DB/Tables/Hero.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Hero]
+(
+	[Id_Hero] INT NOT NULL IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Endurance] INT NOT NULL,
+	[Strength] INT NOT NULL,
+	[Id_Race] INT NOT NULL,
+
+	CONSTRAINT PK_Hero 
+		PRIMARY KEY([Id_Hero]),
+
+	CONSTRAINT FK_Hero__Race 
+		FOREIGN KEY ([Id_Race])
+		REFERENCES [Race]([Id_Race])
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+)
---
+++ ASP_MVC_03_Modele.DB/Tables/Race.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Race]
+(
+	[Id_Race] INT NOT NULL IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Endurance_Modifier] INT NOT NULL DEFAULT 0,
+	[Strength_Modifier] INT NOT NULL DEFAULT 0,
+
+	CONSTRAINT PK_Race PRIMARY KEY ([Id_Race]),
+	CONSTRAINT UK_Race__Name UNIQUE ([Name])
+)
