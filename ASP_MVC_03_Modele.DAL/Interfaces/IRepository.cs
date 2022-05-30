c51fcd56ba74553dce099799d8715480a387cf13
Aurélien S <aurelien.strimelle@bstorm.be>
2022-05-30 14:05:31 +0200

    Interface générique du Repository

---
+++ ASP_MVC_03_Modele.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_03_Modele.DAL.Interfaces
+{
+    public interface IRepository<TKey, TEntity>
+    {
+        // Create
+        TKey Insert(TEntity entity);
+
+        // Read
+        TEntity GetById(TKey id);
+        IEnumerable<TEntity> GetAll();
+
+        // Update
+        bool Update(TKey id, TEntity entity);
+
+        // Delete
+        bool Delete(TKey id);
+    }
+}
