1d7ff116ef53b4370f1ce893003bd1ec251463fb
Aurélien S <aurelien.strimelle@bstorm.be>
2022-05-30 12:15:44 +0200

    Initial Data

---
+++ ASP_MVC_03_Modele.DB/Scripts/Seed.sql
@@ -0,0 +1,19 @@
+﻿USE [HeroesVsMonster]
+GO
+
+-- Race
+SET IDENTITY_INSERT [Race] ON;
+GO
+
+INSERT INTO [Race] ([Id_Race], [Name], [Endurance_Modifier], [Strength_Modifier])
+ VALUES (1, N'Humain', 1, 1),
+		(2, N'Nain', 2, 0);
+GO
+
+SET IDENTITY_INSERT [Race] OFF;
+GO
+
+-- Hero
+INSERT INTO [Hero] ([Name], [Endurance], [Strength], [Id_Race])
+ VALUES (N'Dominique', 10, 16, 1),
+		(N'Thierry', 18, 10, 2);
