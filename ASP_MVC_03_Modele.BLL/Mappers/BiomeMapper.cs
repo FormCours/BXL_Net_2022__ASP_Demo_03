9830ca3e5aa234308a177f651bb6599bec529f1f
Aurélien S <aurelien.strimelle@bstorm.be>
2022-06-01 16:12:20 +0200

    Add BLL Service for Biome

---
+++ ASP_MVC_03_Modele.BLL/Mappers/BiomeMapper.cs
@@ -0,0 +1,39 @@
+﻿using ASP_MVC_03_Modele.BLL.DTO;
+using ASP_MVC_03_Modele.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_03_Modele.BLL.Mappers
+{
+    internal static class BiomeMapper
+    {
+
+        public static BiomeDTO ToDTO(this BiomeEntity entity)
+        {
+            return new BiomeDTO()
+            {
+                IdBiome = entity.IdBiome,
+                Name = entity.Name,
+                Desc = entity.Desc,
+                DifficultyLevel = entity.DifficultyLevel,
+                ImageUri = entity.ImageUri
+            };
+        }
+
+        public static BiomeEntity ToEntity(this BiomeDTO dto)
+        {
+            return new BiomeEntity()
+            {
+                IdBiome = dto.IdBiome,
+                Name = dto.Name,
+                Desc = dto.Desc,
+                DifficultyLevel = dto.DifficultyLevel,
+                ImageUri = dto.ImageUri
+            };
+        }
+
+    }
+}
---
+++ ASP_MVC_03_Modele.BLL/Sercices/BiomeService.cs
@@ -0,0 +1,43 @@
+﻿using ASP_MVC_03_Modele.BLL.DTO;
+using ASP_MVC_03_Modele.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASP_MVC_03_Modele.BLL.Mappers;
+using ASP_MVC_03_Modele.DAL.Interfaces;
+
+namespace ASP_MVC_03_Modele.BLL.Sercices
+{
+    public class BiomeService
+    {
+        private IBiomeRepository biomeRepository;
+
+        public BiomeService(IBiomeRepository biomeRepository)
+        {
+            this.biomeRepository = biomeRepository;
+        }
+
+
+        public BiomeDTO GetById(int id)
+        {
+            return biomeRepository.GetById(id)?.ToDTO();
+        }
+
+        public IEnumerable<BiomeDTO> GetAll()
+        {
+            return biomeRepository.GetAll().Select(b => b.ToDTO());
+        }
+
+        public BiomeDTO Insert(BiomeDTO biome)
+        {
+            int idBiome = biomeRepository.Insert(biome.ToEntity());
+
+            biome.IdBiome = idBiome;
+            return biome;
+        }
+
+    }
+}
