f89b53c10434bf235e4879ec3be30ea37b0564b1
Aurélien S <aurelien.strimelle@bstorm.be>
2022-06-01 12:13:29 +0200

    Repository "Biome" done

--- ASP_MVC_03_Modele.DAL/Repositories/BiomeRepository.cs
+++ ASP_MVC_03_Modele.DAL/Repositories/BiomeRepository.cs
@@ -18,17 +18,46 @@ namespace ASP_MVC_03_Modele.DAL.Repositories
 
         public override int Insert(BiomeEntity entity)
         {
-            throw new NotImplementedException();
+            Command cmd = new Command($"INSERT INTO {TableName}(Name, Description, Difficulty_Level, Image_Uri)" +
+                $" OUTPUT inserted.{TableId}" +
+                " VALUES (@Name, @Description, @Difficulty_Level, @Image_Uri)");
+
+            cmd.AddParameter("Name", entity.Name);
+            cmd.AddParameter("Description", entity.Desc);
+            cmd.AddParameter("Difficulty_Level", entity.DifficultyLevel);
+            cmd.AddParameter("Image_Uri", entity.ImageUri);
+
+            return (int)_Connection.ExecuteScalar(cmd);
         }
 
         public override bool Update(int id, BiomeEntity entity)
         {
-            throw new NotImplementedException();
+            Command cmd = new Command($"UPDATE {TableName} " +
+                " SET Name = @Name," +
+                "     Description = @Desc," +
+                "     Difficulty_Level = @Diff_Level" +
+                "     Image_Uri = @Image_Uri" +
+                $" WHERE {TableId} = @Id");
+
+            cmd.AddParameter("Id", id);
+            cmd.AddParameter("Name", entity.Name);
+            cmd.AddParameter("Desc", entity.Desc);
+            cmd.AddParameter("Diff_Level", entity.DifficultyLevel);
+            cmd.AddParameter("Image_Uri", entity.ImageUri);
+
+            return _Connection.ExecuteNonQuery(cmd) == 1; 
         }
 
         protected override BiomeEntity MapRecordToEntity(IDataRecord record)
         {
-            throw new NotImplementedException();
+            return new BiomeEntity()
+            {
+                IdBiome = (int)record[TableId],
+                Name = (string)record["Name"],
+                Desc = record["Description"] is DBNull ? null : (string)record["Description"],
+                DifficultyLevel = (int)record["Difficulty_Level"],
+                ImageUri = record["Image_Uri"] is DBNull ? null : (string)record["Image_Uri"]
+            };
         }
     }
 }
